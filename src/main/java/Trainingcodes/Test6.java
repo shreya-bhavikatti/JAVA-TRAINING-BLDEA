1878434a2ede6a4baaa6593f2a2c33e3a9c7e6ce
Administrator <Administrator@DESKTOP-CJ2TM4F>
2023-03-20 19:01:57 +0530

    sixthcommit

---
+++ src/main/java/Trainingcodes/Test6.java
@@ -0,0 +1,17 @@
+package Trainingcodes;
+
+public class Test6 {
+	 
+			public static void main(String[] args) {
+				for(int i=0;i<=5;i++) {
+					for(int j=0;j<=5;j++) {
+						System.out.print("# ");
+					}
+					System.out.println();
+				}
+			}
+
+	 }
+
+
+
