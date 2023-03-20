6604956e71e1c528b7af19db2ddf853a26b3b9ef
Administrator <Administrator@DESKTOP-CJ2TM4F>
2023-03-20 19:16:17 +0530

    code4commit

---
+++ src/main/java/Trainingcodes/Code4.java
@@ -0,0 +1,97 @@
+package Trainingcodes;
+
+import java.util.Scanner;
+
+public class Code4 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the value of n");
+		int n= sc.nextInt();
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				if((i==0 && j<=n/2 && j!=0 )||
+						(i==(n/2) && j<n/2 && j!=0) || 
+						(i==(n-1) && j<n/2) ||
+						(j==0 && i<n/2 && i!=0) ||
+						(j==n/2 && i>n/2 && i!=(n-1))) {
+						System.out.print("* ");
+					}
+
+					else {
+						System.out.print("  ");
+						
+						
+				}
+			}
+			for(int j=0;j<n;j++) {
+				if((j==0||j==(n-1))||(i==n/2)) {
+					System.out.print("* ");
+				}
+
+				else {
+					System.out.print("  ");
+					
+					
+			}
+		}
+			for(int j=0;j<n;j++) {
+				if((j==0)|| ( i==0 && j<(n/2)) || (j==(n/2) && (i!=0) && (i!= (n/2)) && (i<(n/2))||
+						(i==(n/2)) && (j<(n/2)) || (i-j==(n/2)))) {
+					System.out.print("* ");
+				}
+
+				else {
+					System.out.print("  ");
+					
+					
+			}
+		}
+
+			for(int j=0;j<n;j++) {
+				if(j==0||(i==0)|| (i==(n-1)) ||(i==n/2 && j<=n/2)) {
+					System.out.print("* ");
+				}
+
+				else {
+					System.out.print("  ");
+					
+					
+			}
+		}
+			for(int j=0;j<n;j++) {
+				if((i==j&& i<=(n/2)) || (i+j==(n-1))) {
+					System.out.print("* ");
+				}
+
+				else {
+					System.out.print("  ");
+					
+					
+			}
+		}
+
+			for(int j=0;j<n;j++) {
+				if(i==0 ||  j==0 || j==(n-1) ||
+						i==(n/2)) {
+					System.out.print("* ");
+				}
+
+				else {
+					System.out.print("  ");
+					
+					
+			}
+		}
+			
+				System.out.println();	
+				}
+				}
+					
+				}
+			
+			
+			
+
+
+	
