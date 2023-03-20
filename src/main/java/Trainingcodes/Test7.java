068407d5762cd3438a0e6a80275de693d5e714de
Administrator <Administrator@DESKTOP-CJ2TM4F>
2023-03-20 19:05:21 +0530

    seventhcommit

---
+++ src/main/java/Trainingcodes/Test7.java
@@ -0,0 +1,27 @@
+package Trainingcodes;
+
+import java.util.Scanner;
+public class Test7 {
+
+	public static void main(String[] args) {
+
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the value of n");
+		int n= sc.nextInt();
+		int count;
+		for(int i=1;i<=n;i++) {
+			count=i;
+			for(int j=1;j<=n;j++) {
+				System.out.print(count+" ");
+				count= count+5;
+			}
+			System.out.println();
+		}
+	}
+
+
+}
+
+
+
+
