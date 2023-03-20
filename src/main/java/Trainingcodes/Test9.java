c4d8b5ed0506f18402cef54d1919895e67d39889
Administrator <Administrator@DESKTOP-CJ2TM4F>
2023-03-20 19:09:34 +0530

    ninthcommit

---
+++ src/main/java/Trainingcodes/Test9.java
@@ -0,0 +1,27 @@
+package Trainingcodes;
+
+import java.util.Scanner;
+
+public class Test9 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the value of n");
+		int n= sc.nextInt();
+		int count=n*n;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+				System.out.print(count+" ");
+				count--;
+				
+			}
+			System.out.println();
+			
+
+		}
+
+	}
+
+	}
+
+
