17f8166e008d6e72dc2b10acdaea953f4574fffd
shrey <shrey@Hp>
2023-04-13 14:07:27 +0530

    anagram1

--- src/main/java/Trainingcodes/Anagram.java
+++ src/main/java/Trainingcodes/Anagram.java
@@ -2,20 +2,21 @@ package Trainingcodes;
 import java.util.*;
 public class Anagram {
 public static void main(String args[]) {
-	Scanner sc= new Scanner(System.in);
-	System.out.println("String-1");
-	String s1=sc.next();
-	System.out.println("String-2");
-	String s2=sc.next();
-	char[] c1= s1.toCharArray();
-	char[] c2= s2.toCharArray();
-	Arrays.sort(c1);
-	Arrays.sort(c2);
-	if(Arrays.equals(c1, c2)) {
-		System.out.println("Anagrams");
-	}
-	else {
-		System.out.println("Not Anagrams");
+	try (Scanner sc = new Scanner(System.in)) {
+		System.out.println("String-1");
+		String s1=sc.next();
+		System.out.println("String-2");
+		String s2=sc.next();
+		char[] c1= s1.toCharArray();
+		char[] c2= s2.toCharArray();
+		Arrays.sort(c1);
+		Arrays.sort(c2);
+		if(Arrays.equals(c1, c2)) {
+			System.out.println("Anagrams");
+		}
+		else {
+			System.out.println("Not Anagrams");
+		}
 	}
 }
 }
